2753041425c89dd0ade66cd626b87dad948340c6
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-26 07:04:49 +0430

    Add all files

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    renderHeaderFooter: './src/modules/render_header_footer.js',
+    popup: './src/modules/popup.js',
+    home: './src/modules/home_page.js',
+    display: './src/modules/displayComments.js',
+    counterComment: './src/modules/counterComment.js',
+    likes: './src/modules/likes.js',
+    addComment: './src/modules/addComment.js',
+    items_counter: './src/modules/items_counter.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My meals',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
